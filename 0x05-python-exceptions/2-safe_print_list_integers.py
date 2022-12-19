d4bb956180524b3d88162ece87ba403e0f9d532b
ArnoldG8 <arnoldaijuka@gmail.com>
2022-12-19 10:05:30 -0800

    2. Print and count integers

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    i = printed = 0
+    while True:
+        try:
+            if i < x:
+                print("{:d}".format(my_list[i]), end='')
+                i+=1
+                printed +=1
+            else:
+                print()
+                return printed
+        except (ValueError, TypeError):
+            i += 1
