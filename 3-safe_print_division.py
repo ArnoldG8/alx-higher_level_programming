a788abb56b7da724e523aa019060c2150d4eba08
Arnold <55125494+ArnoldG8@users.noreply.github.com>
2022-12-20 00:31:16 +0600

    Deleting accidental push

--- 3-safe_print_division.py
+++
@@ -1,9 +0,0 @@
-def safe_print_division(a, b):
-    try:
-        result = a/b
-        print("Inside result: {:.1f}".format(result))
-    except:
-        result = None
-        print("Inside result: {}".format(result))
-    finally:
-        return result
