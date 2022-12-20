9f17e8695347d869e4f56bb1cbef78320abced04
ArnoldG8 <arnoldaijuka@gmail.com>
2022-12-20 13:22:58 -0800

    3. Area of a square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -13,3 +13,7 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+    """Defining public instance method that return the square"""
+    def area(self):
+        """Initiliazing an are function that return the area"""
+        return (self.__size**2)
